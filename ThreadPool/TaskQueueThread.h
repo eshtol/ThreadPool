2f81634509240fbae79ff1a2f011ddd119b5ec1e
Shtol Eduard <djcappotto@gmail.com>
2020-07-29 14:05:35 +0300

    TaskQueueThread used without detaching and got appropriate destructor.

--- ThreadPool/TaskQueueThread.h
+++ ThreadPool/TaskQueueThread.h
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <functional>
+#include <atomic>
 #include "ConcurrentContainers.h"
 
 
@@ -10,33 +11,51 @@ template <typename TaskType, template <typename> typename PtrT> class TaskQueueT
 {
 	public:
 		typedef PtrT<TaskType> TaskPtr;
-		TaskQueueThread() { std::thread(ThreadLoop).detach(); }
-		~TaskQueueThread() = default;  // NO
+		TaskQueueThread() { thread = std::thread(ThreadLoop); }
+
+		~TaskQueueThread() 
+		{
+			AcceptTask(PtrT<StopThreadTask>(new StopThreadTask(stop_flag)));
+			thread.join(); 
+		};
 
 		inline bool IsFree() const { return !current_task; }
 
 		inline void WaitTaskForFinished() const { while (!IsFree()) Sleep(50); }
 
-		void AcceptTask(const TaskPtr&& task)
+		void AcceptTask(const TaskPtr task)
 		{
-			m_queue.emplace(task);
+			queue.emplace(task);
 			have_task.notify_one();
 		}
 
 	private:
+		std::thread thread;
 		std::mutex task_mtx;
 		std::condition_variable have_task;
-		concurrent_queue<TaskPtr> m_queue;
+		concurrent_queue<TaskPtr> queue;  //todo: use priority queue.
 		TaskPtr current_task;
+		std::atomic_bool stop_flag = false;
+
+		struct StopThreadTask : TaskType
+		{
+			std::atomic_bool& stop_flag;
+			void execute() override { stop_flag = true; }
+			StopThreadTask(std::atomic_bool& flg) noexcept : stop_flag(flg) {}
+		};
 
 		std::function<void()> ThreadLoop = [&]()
 		{
 			std::unique_lock<decltype(task_mtx)> task_lock(task_mtx);
-			const auto have_task_pred = [this]() { return m_queue.size(); };
-			while (true)
-				have_task.wait(task_lock, have_task_pred),
-				(current_task = m_queue.extract_first())->execute(),
+			const auto have_task_pred = [this]() { return queue.size(); };
+			while (!stop_flag)
+			{
+				have_task.wait(task_lock, have_task_pred);
+				current_task = queue.extract_first();
+				try { current_task->execute(); }
+				catch (const std::exception&) { /*forward somewhere*/ }
 				current_task.reset();
+			}
 		};
 
 		static inline void Sleep(const std::size_t ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }  // NO!!!!
